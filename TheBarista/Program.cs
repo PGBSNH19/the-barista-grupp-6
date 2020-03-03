7032c0c3b8abfa773c780800e111481aaa3beedc
Pierre Nygård <pierre.nygard@gmail.com>
2020-03-03 13:37:38 +0100

    Driver @pierre-nygard

---
+++ TheBarista/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TheBarista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+
+    class EspressoMachine
+    {
+        float Bar { get; set; }
+    }
+
+    class Espresso
+    {
+
+        Type
+        Volume
+
+        enum Type
+        {
+
+        }
+    }
+
+    class Bean
+    {
+        string Country { get; set; }
+        int Strength { get; set; }
+        bool Fairtrade { get; set; }
+        bool Ecologic { get; set; }
+    }
+
+    class Cup
+    {
+        float Volume { get; set; }
+        string Material { get; set; }
+        string Color { get; set; }
+    }
+}
